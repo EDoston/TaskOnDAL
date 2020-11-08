681fe399ceb0393a421445fb39be5b3366bb4e6a
EDoston <edoston58@gmail.com>
2020-11-08 16:57:35 +0300

    Create data states

---
+++ Models/StateData.cs
@@ -0,0 +1,11 @@
+namespace ParsedData.Models
+{
+    public enum StateData
+    {
+        NotStarted = 1,
+        InProcess,
+        Done,
+        Canceled,
+        Refused
+    }
+}
--- Models/TaskData.cs
+++ Models/TaskData.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace ParsedData.Models
 {
     public class TaskData
